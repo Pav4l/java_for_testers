1822a3a9408b28cdf5e1d7a03346af944cebf2bc
Pavel Gorev <pavel.gorev@gmail.com>
2024-03-17 22:58:13 +0300

    Задание №9: Параметризовать тесты для создания групп и контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,27 +1,64 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase{
-
-    @Test
-    public void canCreateContactWithEmptyName() {
-        //openNewContactPage();
-        app.contacts().createContact(new ContactData());
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>(List.of());
+        for (var firstname : List.of("", "firstName")){
+            for (var lastname : List.of("", "lastName")){
+                for (var address: List.of("", "address")){
+                    for (var mobileTelephone: List.of("", "mobileTelephone")){
+                        for (var email: List.of("", "email")){
+                            result.add(new ContactData(firstname, lastname, address, mobileTelephone, email));
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++){
+            result.add(new ContactData(randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
     }
 
-    @Test
-    public void canCreateContactWithOnlyName() {
-        //openNewContactPage();
-        app.contacts().createContact(new ContactData().withName("Pavel"));
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
+        int contactCount = app.contacts().getContactsCount();
+        app.contacts().createContact(contact);
+        int newContactCount = app.contacts().getContactsCount();
+        Assertions.assertEquals(contactCount + 1, newContactCount);
     }
 
-    @Test
-    public void canCreateContact() {
-        //openNewContactPage();
-        app.contacts().createContact(new ContactData("Ivan", "Ivanovich", "Ivanov", "Vanyusha",
-                "Qa Engineer", "TestCompany", "TestAddress", "11111", "22222", "333333",
-                "44444", "Ivan@test.test", "Ivan2@test.test", "Ivan3@test.test", "HomePage"));
+    public static List<ContactData> negativeContactProvider() {
+        var result = new ArrayList<ContactData>(List.of(new ContactData("lastname'", "firstname", "", "", "")));
+        return result;
     }
+
+    @ParameterizedTest
+    @MethodSource("negativeContactProvider")
+    public void cantCreateContacts(ContactData contact) {
+        int contactCount = app.contacts().getContactsCount();
+        app.contacts().createContact(contact);
+        int newContactCount = app.contacts().getContactsCount();
+        Assertions.assertEquals(contactCount, newContactCount);
+    }
+
+    //@Test
+    //public void canCreateContactWithOnlyName() {
+    //    app.contacts().createContact(new ContactData().withName("Pavel"));
+   // }
+
+    //@Test
+    //public void canCreateContact() {
+    //    app.contacts().createContact(new ContactData("Ivan", "Ivanov","TestAddress", "11111", "Ivan@test.test"));
+    //}
 }
--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,16 +1,26 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactRemovalTests extends TestBase{
 
     @Test
     public void canRemovalContactTests() {
-        if (!app.contacts().isContactPresent()) {
-            app.contacts().createContact(new ContactData());
+        if (app.contacts().getContactsCount() == 0) {
+            app.contacts().createContact(new ContactData("firstname","","","",""));
         }
         app.contacts().removeContact();
+    }
+
+    @Test
+    public void canRemovalAllContactsTests() {
+        if (app.contacts().getContactsCount() == 0) {
+            app.contacts().createContact(new ContactData("firstname","","","",""));
+        }
+        app.contacts().removeAllContacts();
+        Assertions.assertEquals(0,app.contacts().getContactsCount());
     }
 }
 
