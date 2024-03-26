f2d7605f0480d950e85bfad8a1084180e57c7b6c
Pavel Gorev <pavel.gorev@gmail.com>
2024-03-26 23:53:24 +0300

    Задание 10, 11. Реализовать проверки путём сравнения списков для создания, модификации и удаления групп и контактов.

--- addressbook_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -3,9 +3,12 @@ package manager;
 import model.ContactData;
 import org.openqa.selenium.By;
 
-public class ContactHelper extends HelperBase{
+import java.util.ArrayList;
+import java.util.List;
 
-    public ContactHelper(ApplicationManager manager){
+public class ContactHelper extends HelperBase {
+
+    public ContactHelper(ApplicationManager manager) {
         super(manager);
     }
 
@@ -27,19 +30,28 @@ public class ContactHelper extends HelperBase{
     }
 
     public int getContactsCount() {
+        openHomePage();
         return manager.driver.findElements(By.name("selected[]")).size();
     }
 
-    public void removeContact() {
-        manager.driver.findElement(By.name("selected[]")).click();
-        manager.driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
-        manager.driver.findElement(By.cssSelector("html")).click();
-        //manager.driver.switchTo().alert().accept();
+    private void openHomePage() {
+        click(By.linkText("home"));
+    }
+
+    public void removeContact(ContactData contact) {
+        openHomePage();
+        selectContact(contact);
+        removeSelectedContact();
+        openHomePage();
     }
 
     public void removeAllContacts() {
         selectAllContacts();
         removeSelectedContact();
+    }
+
+    private void selectContact(ContactData contact) {
+        click(By.cssSelector(String.format("input[value='%s']", contact.id())));
     }
 
     private void selectAllContacts() {
@@ -50,7 +62,49 @@ public class ContactHelper extends HelperBase{
     }
 
     private void removeSelectedContact() {
-        click(By.xpath("//input[@value=\'Delete\']"));
+        click(By.xpath("//input[@value='Delete']"));
     }
+
+    public List<ContactData> getContactList() {
+        openHomePage();
+        var contacts = new ArrayList<ContactData>();
+        var tds = manager.driver.findElements(By.name("entry"));
+        for (var td : tds) {
+            var checkbox = td.findElement(By.className("center")).findElement(By.name("selected[]"));
+            var id = checkbox.getAttribute("value");
+            var name = td.findElement(By.cssSelector("td:nth-child(3)")).getText();
+            var lastName = td.findElement(By.cssSelector("td:nth-child(2)")).getText();
+            contacts.add(new ContactData().withId(id).withName(name).withLastName(lastName));
+        }
+        return contacts;
+    }
+
+    public void modifyContact(ContactData contact, ContactData modifiedContact) {
+        openHomePage();
+        selectContact(contact);
+        initContactModification(contact);
+        fillContactForm(modifiedContact);
+        submitContactModification();
+        openHomePage();
+    }
+
+    private void initContactModification(ContactData contact) {
+        click(By.cssSelector(String.format("a[href*='edit.php?id=%s']", contact.id())));
+        //click(By.name("edit"));
+
+    }
+
+    private void fillContactForm(ContactData contact) {
+        type(By.name("firstname"), contact.firstName());
+        type(By.name("lastname"), contact.lastName());
+        type(By.name("address"), contact.address());
+        type(By.name("mobile"), contact.mobileTelephone());
+        type(By.name("email"), contact.email());
+    }
+
+    private void submitContactModification() {
+        click(By.name("update"));
+    }
+
 
 }
--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -2,29 +2,20 @@ package tests;
 
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase{
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>(List.of());
-        for (var firstname : List.of("", "firstName")){
-            for (var lastname : List.of("", "lastName")){
-                for (var address: List.of("", "address")){
-                    for (var mobileTelephone: List.of("", "mobileTelephone")){
-                        for (var email: List.of("", "email")){
-                            result.add(new ContactData(firstname, lastname, address, mobileTelephone, email));
-                        }
-                    }
-                }
-            }
-        }
         for (int i = 0; i < 5; i++){
-            result.add(new ContactData(randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+            result.add(new ContactData()
+                    .withName(randomString(i * 10))
+                    .withLastName(randomString(i * 10)));
         }
         return result;
     }
@@ -32,14 +23,24 @@ public class ContactCreationTests extends TestBase{
     @ParameterizedTest
     @MethodSource("contactProvider")
     public void canCreateMultipleContacts(ContactData contact) {
-        int contactCount = app.contacts().getContactsCount();
+        var oldContacts = app.contacts().getContactList();
         app.contacts().createContact(contact);
-        int newContactCount = app.contacts().getContactsCount();
-        Assertions.assertEquals(contactCount + 1, newContactCount);
+        var newContacts = app.contacts().getContactList();
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
+        newContacts.sort(compareById);
+        var expectedList = new ArrayList<>(oldContacts);
+        expectedList.add(contact
+                .withId(newContacts.get(newContacts.size() - 1).id())
+                .withName(contact.firstName())
+                .withLastName(contact.lastName()));
+        expectedList.sort(compareById);
+        Assertions.assertEquals(newContacts, expectedList);
     }
 
     public static List<ContactData> negativeContactProvider() {
-        var result = new ArrayList<ContactData>(List.of(new ContactData("lastname'", "firstname", "", "", "")));
+        var result = new ArrayList<ContactData>(List.of(new ContactData("", "firstname", "lastName'","", "", "")));
         return result;
     }
 
