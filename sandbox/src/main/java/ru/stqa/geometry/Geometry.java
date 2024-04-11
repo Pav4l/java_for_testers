aa483b9811fc40fc922e4de97a13686fea2f4a54
Pavel Gorev <pavel.gorev@gmail.com>
2024-04-12 00:58:57 +0300

    Лекция 7.2. Функции-генераторы, или сапплаеры (Supplier)

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -5,16 +5,23 @@ import ru.stqa.geometry.figures.Square;
 import ru.stqa.geometry.figures.Triangle;
 
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Geometry {
     public static void main(String[] args) {
-        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
+        Supplier<Square> randomSquare = () -> new Square(new Random().nextDouble(100.0));
+        var squares = Stream.generate(randomSquare).limit(5);
 //        for (Square square : squares) {
 //            Square.printSquareArea(square);
 //        }
-        //Consumer<Square> print = Square::printSquareArea;
-        squares.forEach(Square::printSquareArea);
+//        Consumer<Square> print = square -> {
+//            Square.printSquareArea(square);
+//            Square.printPerimeterArea(square);
+//        };
+        squares.peek(Square::printSquareArea).forEach(Square::printPerimeterArea);
 
 
 //        Rectangle.printRectangleArea(3.0, 5.0);
