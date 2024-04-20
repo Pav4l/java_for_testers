65a79d20f3e69da5068eaa164b30f05148b38367
Pavel Gorev <pavel.gorev@gmail.com>
2024-04-20 23:40:25 +0300

    Задание №18: Реализовать сценарий регистрации нового пользователя в MantisBT

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/SessionHelper.java
@@ -1,6 +1,10 @@
 package ru.stqa.mantis.manager;
 
 import org.openqa.selenium.By;
+import ru.stqa.mantis.model.MailMessage;
+
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class SessionHelper extends HelperBase{
 
@@ -8,14 +12,29 @@ public class SessionHelper extends HelperBase{
         super(manager);
     }
 
-    public void login(String user, String password) {
+    public void signUp(String user, String email) {
+        click(By.linkText("Signup for a new account"));
         type(By.name("username"), user);
-        click(By.cssSelector("input[type='submit']"));
-        type(By.name("password"), password);
+        type(By.name("email"), email);
         click(By.cssSelector("input[type='submit']"));
     }
 
-    public boolean isLoggedIn() {
-        return isElementPresent(By.cssSelector("span.user-info"));
+    public String getConfirmLink(List<MailMessage> messages) {
+        var text = messages.get(0).content();
+        var pattern = Pattern.compile("http://\\S*");
+        var matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            return text.substring(matcher.start(), matcher.end());
+        } else {
+            throw new IllegalArgumentException("Url not found");
+        }
+    }
+
+    public void finishRegistration(String url, String user, String password) {
+        manager.driver().get(url);
+        type(By.name("realname"), user);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("button[type='submit']"));
     }
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -1,17 +1,31 @@
 package ru.stqa.mantis.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.stqa.mantis.common.CommonFunctions;
+
+import java.time.Duration;
 
 public class UserRegistrationTests extends TestBase{
 
     @Test
-    void canRegisterUser(String username){
+    void canRegisterUser(){
+        var username = CommonFunctions.randomString(5);
+        var password = "password";
         var email = String.format("%s@localhost", username);
-        // создать пользователя (адрес) на почтовом сервере (JamesHlper)
+
+        // создать пользователя (адрес) на почтовом сервере (JamesHelper)
+        app.jamesCli().addUser(email, password);
         // заполняем форму создания и отправляем (браузер)
+        app.session().signUp(username, email);
         // ждём почту (MailHelper)
+        var messages = app.mail().receive(email, password, Duration.ofSeconds(15));
         // извлекаем ссылку из письма
+        var url = app.session().getConfirmLink(messages);
         // проходим по ссылке и завершаем регистрацию пользователя (браузер)
+        app.session().finishRegistration(url, username, password);
         // проверяем, что пользователь может залогиниться (HttpSessionHelper)
+        app.http().login(username, password);
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
